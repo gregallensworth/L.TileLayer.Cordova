8c44d4f20dbb851086dcf3755dfec529b70d3b6d
Greg Allensworth <you@example.com>
2015-03-21 11:51:35 -0700

    Per issue #4 some cosmetic layout toward new demo UI

--- test/www/index.js
+++ test/www/index.js
@@ -4,12 +4,13 @@ var ZOOM = 13;
 
 var CACHE_ZOOM_MIN = 13, CACHE_ZOOM_MAX = 16;
 
-var OVERWRITE = true;
-
 var MAP, BASE;
 
-function startCordovaTileTest() {
-    MAP = L.map('map');
+function startMap() {
+    MAP = L.map('map', {
+        minZoom:CACHE_ZOOM_MIN,
+        maxZoom:CACHE_ZOOM_MAX
+    });
 
     try {
         BASE = L.tileLayerCordova('https://{s}.tiles.mapbox.com/v3/examples.map-i875mjb7/{z}/{x}/{y}.png', {
@@ -28,24 +29,35 @@ function startCordovaTileTest() {
     }
 
     MAP.setView([LAT,LNG],ZOOM);
-    document.getElementById('status').innerHTML =  LAT + ' x ' + LNG + ' @ ' + ZOOM;
-
-    setTimeout(startCachingTest, 3000);
+    MAP.on('moveend', function () {
+        document.getElementById('status').innerHTML =  MAP.getCenter().lat.toFixed(5) + ' x ' + MAP.getCenter().lng.toFixed(5) + ' @ ' + MAP.getZoom();
+    }).fire('moveend');
 }
 
-function startCachingTest() {
-    var status_block = document.getElementById('status');
-    var tile_list = BASE.calculateXYZListFromPyramid(LAT, LNG, CACHE_ZOOM_MIN, CACHE_ZOOM_MAX);
+function startButtons() {
+    document.getElementById('test_cache').addEventListener('click', testCaching);
+    document.getElementById('test_offline').addEventListener('click', testOffline);
+    document.getElementById('test_online').addEventListener('click', testOnline);
+    document.getElementById('test_cache').addEventListener('click', testUsage);
+}
 
-    var message = "Preparing to cache tiles." + "<br/>" + "Zoom level " + CACHE_ZOOM_MIN + " through " + CACHE_ZOOM_MAX + "<br/>" + tile_list.length + " tiles total.";
-    console.log(message);
-    status_block.innerHTML = message;
+function testCaching() {
+    var lat       = MAP.getCenter().lat;
+    var lng       = MAP.getCenter().lng;
+    var zmin      = MAP.getZoom();
+    var zmax      = CACHE_ZOOM_MAX;
+    var tile_list = BASE.calculateXYZListFromPyramid(lat, lng, zmin, zmax);
+    var message   = "Preparing to cache tiles.\n" + "Zoom level " + zmin + " through " + zmax + "\n" + tile_list.length + " tiles total." + "\nClick OK to proceed.";
+    var ok        = confirm(message);
+    if (! ok) return false;
+
+    var status_block = document.getElementById('status');
 
     BASE.downloadXYZList(
         // 1st param: a list of XYZ objects indicating tiles to download
         tile_list,
         // 2nd param: overwrite existing tiles on disk? if no then a tile already on disk will be kept, which can be a big time saver
-        OVERWRITE,
+        false,
         // 3rd param: progress callback
         // receives the number of tiles downloaded and the number of tiles total; caller can calculate a percentage, update progress bar, etc.
         function (done,total) {
@@ -55,11 +67,8 @@ function startCachingTest() {
         // 4th param: complete callback
         // no parameters are given, but we know we're done!
         function () {
-            // for this demo, on success we use another L.TileLayer.Cordova feature and show the disk usage!
-            BASE.getDiskUsage(function (filecount,bytes) {
-                var kilobytes = Math.round( bytes / 1024 );
-                status_block.innerHTML = "Done" + "<br/>" + filecount + " files" + "<br/>" + kilobytes + " kB";
-            });
+            // for this demo, on success we use another L.TileLayer.Cordova feature and show the disk usage
+            testUsage();
         },
         // 5th param: error callback
         // parameter is the error message string
@@ -67,4 +76,19 @@ function startCachingTest() {
             alert("Failed\nError code: " + error.code);
         }
     );
+}
+
+function testOffline() {
+
+}
+
+function testOnline() {
+
+}
+
+function testUsage() {
+    BASE.getDiskUsage(function (filecount,bytes) {
+        var kilobytes = Math.round( bytes / 1024 );
+        status_block.innerHTML = "Done" + "<br/>" + filecount + " files" + "<br/>" + kilobytes + " kB";
+    });
 }
